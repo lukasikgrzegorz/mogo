136540d8fb03b3aceed753c251456c0d6e7860d1
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2023-01-07 11:29:40 +0100

    aria and retina

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,1 @@
-const menu = document.querySelector(".mobile-menu");
-const openBtn = document.querySelector(".header__menu-button");
-const closeBtn = document.querySelector(".mobile-menu__close-button");
-
-const toggleMenu = () => {
-	menu.classList.toggle("visible");
-};
-
-openBtn.addEventListener("click", toggleMenu);
-closeBtn.addEventListener("click", toggleMenu);
+const menu=document.querySelector(".mobile-menu"),openBtn=document.querySelector(".header__menu-button"),closeBtn=document.querySelector(".mobile-menu__close-button"),toggleMenu=()=>{menu.classList.toggle("visible")};openBtn.addEventListener("click",toggleMenu),closeBtn.addEventListener("click",toggleMenu);
